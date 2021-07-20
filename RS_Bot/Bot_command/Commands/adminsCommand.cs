120ab81c3607e14ee7aae24e979af69863250d18
Ruslan Sadikov <51824085+ruslans-s@users.noreply.github.com>
2021-07-20 08:55:51 +0300

    v0.31
    Пофиксил ошибку если приходит более 1 команды getlog

--- RS_Bot/Bot_command/Commands/adminsCommand.cs
+++ RS_Bot/Bot_command/Commands/adminsCommand.cs
@@ -21,19 +21,39 @@ namespace RS_Bot.Bot_command.Commands
 
         public override async void Execute(Message message, TelegramBotClient client, SqlConnection sql)
         {
+          
             if (message.Chat.Id.ToString() == adminId)
             {
                 await client.SendTextMessageAsync(message.Chat.Id, $"Привет Admin, секунду...");
-                FileStream log = System.IO.File.Open("log.txt", FileMode.Open);
-                await client.SendDocumentAsync(adminId, new InputOnlineFile(log, @"log.txt"));
+                try
+                {
+                    using (var log = System.IO.File.Open("log.txt", FileMode.Open))
+                    {
+                        //Добавляем к старому содержимому файла
+                        await client.SendDocumentAsync(adminId, new InputOnlineFile(log, @"log.txt"));
+                    }
+                }
+                catch (IOException ioex)
+                {
+                    await client.SendTextMessageAsync(message.Chat.Id, $"Файл занят");
+                }
+             
+           
             }
             else
             {
                 await client.SendTextMessageAsync(message.Chat.Id, $"Сам такой");
-                using (var writer = new StreamWriter("log.txt", true))
+                try
                 {
-                    //Добавляем к старому содержимому файла
-                    writer.WriteLine(DateTime.Now + @" До меня домогался: " + message.Chat.Id.ToString() + " " + message.Chat.Username.ToString());
+                    using (var writer = new StreamWriter("log.txt", true))
+                    {
+                        //Добавляем к старому содержимому файла
+                        writer.WriteLine(DateTime.Now + @" До меня домогался: " + message.Chat.Id.ToString() + " " + message.Chat.Username.ToString());
+                    }
+                }
+                catch (IOException ioex)
+                {
+                    await client.SendTextMessageAsync(message.Chat.Id, $"Файл занят");
                 }
             }
         }
